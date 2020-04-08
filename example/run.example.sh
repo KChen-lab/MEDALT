65fe7e11b0c70839f20e33592cd6e6a43ac9795b
Fang Wang <wfang0828@gmail.com>
2020-04-08 09:45:24 -0500

    example update

--- example/run.example.sh
+++ example/run.example.sh
@@ -1,5 +1,5 @@
-python scTree.py -P ./ -I ./example/scDNA.CNV1.txt -O ./example/outputDNA -D D 
-python scTree.py -P ./ -I ./example/scRNA.CNV1.txt -O ./example/outputRNA -D R  
+python scTree.py -P ./ -I ./example/scDNA.CNV.txt -O ./example/outputDNA -D D
+python scTree.py -P ./ -I ./example/scRNA.CNV.txt -O ./example/outputRNA -D R
 
-python scTree.py -P ./ -I ./example/scDNA.CNV1.txt -O ./example/outputDNAT -D D -R T 
-python scTree.py -P ./ -I ./example/scRNA.CNV1.txt -O ./example/outputRNAT -D R -R T 
+python scTree.py -P ./ -I ./example/scDNA.CNV.txt -O ./example/outputDNAT -D D -R T
+python scTree.py -P ./ -I ./example/scRNA.CNV.txt -O ./example/outputRNAT -D R -R T 
